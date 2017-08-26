a30be8b4bdc2ee5eb927cca7b640ef3caba8bab5
histrong <histrong@outlook.it>
2017-08-26 10:05:12 +0800

    ignore elpa folder, and add melpa repository

--- init.el
+++ init.el
@@ -1,9 +1,6 @@
 ;; 显示行号
 (global-linum-mode 1)
 
-;; 关闭启动帮助画面
-;; (setq inhibit-splash-screen 1)
-
 ;; 快速打开配置文件
 (defun open-init-file()
   (interactive)
@@ -11,3 +8,8 @@
 
 ;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f2>") 'open-init-file)
+
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize) ;; You might already have this line
