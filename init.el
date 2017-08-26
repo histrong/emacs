09d55e61a1742ff40ef04c07ff4affed3544da44
histrong <histrong@outlook.it>
2017-08-26 09:55:57 +0800

    add init.el file

---
+++ init.el
@@ -0,0 +1,13 @@
+;; 显示行号
+(global-linum-mode 1)
+
+;; 关闭启动帮助画面
+;; (setq inhibit-splash-screen 1)
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
